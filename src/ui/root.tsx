5f84a31787f4885f6abd8f2c52ed37413eb103c0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-01-12 11:33:44 +0900

    chore: cleanup

--- src/ui/root.tsx
+++ src/ui/root.tsx
@@ -1,6 +1,3 @@
-import { createTinyForm } from "@hiogawa/tiny-form";
-import { tinyassert } from "@hiogawa/utils";
-import { useMutation, useQuery } from "@tanstack/react-query";
 import React from "react";
 import { QueryClientWrapper } from "./query";
 
@@ -123,261 +120,111 @@ function App() {
 		manager.getSnapshot,
 	);
 
-	const registerMutation = useMutation({
-		mutationFn: async () => {
-			const descriptionReady = new Promise((resolve, reject) => {
-				manager.pc.addEventListener(
-					"negotiationneeded",
-					async function handler() {
-						manager.pc.removeEventListener("negotiationneeded", handler);
-						manager.pc.setLocalDescription().then(resolve, reject);
-					},
-				);
-			});
-			const channel = manager.pc.createDataChannel("webrtc-demo");
-			channel.addEventListener("open", (e) => {
-				console.log("[channel.onopen]", e);
-				channel.send("Hello from sender");
-			});
-			channel.addEventListener("message", (e) => {
-				console.log("[channel.onmessage]", e);
-			});
-			await descriptionReady;
-			const res = await fetch("/api/register", {
-				method: "POST",
-				body: JSON.stringify({
-					name: form.data.name,
-					description: manager.pc.localDescription,
-				}),
-			});
-			tinyassert(res.ok);
-		},
-		onSettled() {
-			// discoverQuery.refetch();
-		},
-	});
-
-	const connectMutation = useMutation({
-		mutationFn: async (remoteDescription: RTCSessionDescriptionInit) => {
-			const descriptionReady = new Promise<void>((resolve, _reject) => {
-				manager.pc.addEventListener(
-					"iceconnectionstatechange",
-					function handler() {
-						if (manager.pc.iceConnectionState === "checking") {
-							manager.pc.removeEventListener(
-								"iceconnectionstatechange",
-								handler,
+	return (
+		<div className="flex flex-col gap-4 w-full max-w-lg mx-auto items-start p-2">
+			<h1 className="text-xl">WebRTC Tester</h1>
+			<div className="flex w-full">
+				<div className="flex-1 flex flex-col gap-2 items-center">
+					<h4>Caller</h4>
+					<button
+						onClick={() => {
+							const channel = manager.pc.createDataChannel("webrtc-demo");
+							channel.addEventListener("open", (e) => {
+								console.log("[channel.onopen]", e);
+								channel.send("Hello from sender");
+							});
+							channel.addEventListener("message", (e) => {
+								console.log("[channel.onmessage]", e);
+							});
+						}}
+					>
+						1. createDataChannel
+					</button>
+					<button
+						onClick={() => {
+							manager.pc.setLocalDescription();
+						}}
+					>
+						2. setLocalDescription
+					</button>
+					<button
+						onClick={() => {
+							const result = window.prompt(
+								"Copy 'localDescription' from 'Answer' side",
 							);
-							resolve();
-						}
-					},
-				);
-			});
-			await manager.pc.setRemoteDescription(remoteDescription);
-			await manager.pc.setLocalDescription();
-			await descriptionReady;
-			const res = await fetch("/api/register", {
-				method: "POST",
-				body: JSON.stringify({
-					name: form.data.name,
-					// TODO: needs to wait for signalingstatechange etc...?
-					description: manager.pc.localDescription,
-				}),
-			});
-			tinyassert(res.ok);
-		},
-		onSettled() {
-			// discoverQuery.refetch();
-		},
-	});
-
-	// const discoverQuery = useQuery({
-	// 	queryKey: ["/api/discover"],
-	// 	queryFn: async () => {
-	// 		const res = await fetch("/api/discover");
-	// 		return (await res.json()) as any[];
-	// 	},
-	// 	refetchInterval: 5000,
-	// });
-
-	const form = createTinyForm(
-		React.useState({
-			name: "",
-			remoteDescription: "",
-		}),
-	);
-
-	// debug
-	if (1) {
-		return (
-			<div className="flex flex-col gap-4 w-full max-w-lg mx-auto items-start p-2">
-				<h1 className="text-xl">WebRTC Test</h1>
-				<div className="flex w-full">
-					<div className="flex-1 flex flex-col gap-2 items-center">
-						<h4>Offer</h4>
-						<button
-							onClick={() => {
-								const channel = manager.pc.createDataChannel("webrtc-demo");
+							if (result) {
+								manager.pc.setRemoteDescription(JSON.parse(result));
+							}
+						}}
+					>
+						6. setRemoteDescription
+					</button>
+				</div>
+				<div className="flex-1 flex flex-col gap-2 items-center">
+					<h4>Callee</h4>
+					<button
+						onClick={() => {
+							manager.pc.addEventListener("datachannel", (e) => {
+								const channel = e.channel;
 								channel.addEventListener("open", (e) => {
 									console.log("[channel.onopen]", e);
-									channel.send("Hello from sender");
+									channel.send("Hello from receiver");
 								});
 								channel.addEventListener("message", (e) => {
 									console.log("[channel.onmessage]", e);
 								});
-							}}
-						>
-							1. createDataChannel
-						</button>
-						<button
-							onClick={() => {
-								manager.pc.setLocalDescription();
-							}}
-						>
-							3. setLocalDescription
-						</button>
-						<button
-							onClick={() => {
-								const result = window.prompt(
-									"Copy 'localDescription' from 'Answer' side",
-								);
-								if (result) {
-									manager.pc.setRemoteDescription(JSON.parse(result));
-								}
-							}}
-						>
-							6. setRemoteDescription
-						</button>
-					</div>
-					<div className="flex-1 flex flex-col gap-2 items-center">
-						<h4>Answer</h4>
-						<button
-							onClick={() => {
-								manager.pc.addEventListener("datachannel", (e) => {
-									const channel = e.channel;
-									channel.addEventListener("open", (e) => {
-										console.log("[channel.onopen]", e);
-										channel.send("Hello from receiver");
-									});
-									channel.addEventListener("message", (e) => {
-										console.log("[channel.onmessage]", e);
-									});
-								});
-							}}
-						>
-							2. listen "datachannel"
-						</button>
-						<button
-							onClick={() => {
-								const result = window.prompt(
-									"Copy 'localDescription' from 'Offer' side",
-								);
-								if (result) {
-									manager.pc.setRemoteDescription(JSON.parse(result));
-								}
-							}}
-						>
-							4. setRemoteDescription
-						</button>
-						<button
-							onClick={() => {
-								manager.pc.setLocalDescription();
-							}}
-						>
-							5. setLocalDescription
-						</button>
-					</div>
-				</div>
-				{/* TODO: not needed? */}
-				{/* <button
-					onClick={() => {
-						const result = window.prompt("addIceCandidate");
-						if (result) {
-							manager.pc.addIceCandidate(JSON.parse(result));
-						}
-					}}
-				>
-					addIceCandidate
-				</button> */}
-				<div>
-					<h4>RTCPeerConnection</h4>
-					<pre className="break-all whitespace-pre-wrap">
-						{JSON.stringify(state.connection, null, 2)}
-					</pre>
-				</div>
-				<div>
-					{/* TODO */}
-					<h4>RTCIceCandidate</h4>
-					<pre className="break-all whitespace-pre-wrap">
-						{JSON.stringify([], null, 2)}
-					</pre>
-				</div>
-				{/* TODO: channel message */}
-			</div>
-		);
-	}
-
-	return (
-		<div className="flex flex-col gap-2 w-full max-w-lg">
-			<div className="flex flex-col gap-2">
-				<div>
-					<input {...form.fields.name.props()} />
-					<button
-						onClick={() => {
-							registerMutation.mutate();
+							});
 						}}
 					>
-						Register
+						3. listen "datachannel"
 					</button>
-				</div>
-				<div className="flex flex-col gap-2">
-					<textarea {...form.fields.remoteDescription.props()} />
 					<button
 						onClick={() => {
-							connectMutation.mutate(JSON.parse(form.data.remoteDescription));
+							const result = window.prompt(
+								"Copy 'localDescription' from 'Offer' side",
+							);
+							if (result) {
+								manager.pc.setRemoteDescription(JSON.parse(result));
+							}
 						}}
 					>
-						Connect
+						4. setRemoteDescription
+					</button>
+					<button
+						onClick={() => {
+							manager.pc.setLocalDescription();
+						}}
+					>
+						5. setLocalDescription
 					</button>
 				</div>
-				<pre>{JSON.stringify(state.connection, null, 2)}</pre>
 			</div>
-			{/* <pre>{JSON.stringify(discoverQuery.data, null, 2)}</pre> */}
-			{false && <MockApp />}
-		</div>
-	);
-}
-
-function MockApp() {
-	return (
-		<div className="flex flex-col gap-2 w-full max-w-lg">
-			<div className="flex items-center gap-2">
-				<button>Register</button>
+			{/* TODO: not needed? */}
+			<button
+				hidden
+				onClick={() => {
+					const result = window.prompt("addIceCandidate");
+					if (result) {
+						manager.pc.addIceCandidate(JSON.parse(result));
+					}
+				}}
+			>
+				addIceCandidate
+			</button>
+			<div>
+				<h4>RTCPeerConnection</h4>
+				<pre className="text-xs break-all whitespace-pre-wrap">
+					{JSON.stringify(state.connection, null, 2)}
+				</pre>
 			</div>
-			<table>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Registered at</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>John</td>
-						<td>Sun Jan 05 2025 15:46:55 GMT+0900 </td>
-						<td>
-							<button>Connect</button>
-						</td>
-					</tr>
-					<tr>
-						<td>Joe</td>
-						<td>Sun Jan 05 2025 15:46:55 GMT+0900 </td>
-						<td>
-							<button>Connect</button>
-						</td>
-					</tr>
-				</tbody>
-			</table>
+			<div>
+				{/* TODO */}
+				<h4>RTCIceCandidate</h4>
+				<pre className="text-xs break-all whitespace-pre-wrap">
+					{JSON.stringify([], null, 2)}
+				</pre>
+			</div>
+			{/* TODO: channel message */}
 		</div>
 	);
 }
